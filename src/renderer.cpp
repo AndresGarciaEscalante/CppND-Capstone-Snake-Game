d6be18ea9760607ce13abfb864102addaf4b878f
AndresGarciaEscalante <andres_ge@hotmail.es>
2023-12-23 11:41:30 -0600

    feat: Add the promise future into the render so that snake and food can be map concurrently and also added mutex for do not overriding the plot

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -38,30 +38,25 @@ Renderer::~Renderer() {
   SDL_Quit();
 }
 
-void Renderer::Render(Snake const snake, SDL_Point const &food, Food::FoodQuality const &quality) {
-  SDL_Rect block;
-  block.w = screen_width / grid_width;
-  block.h = screen_height / grid_height;
-
-  // Clear screen
-  SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
-  SDL_RenderClear(sdl_renderer);
-
-  // Render food
+void Renderer::RenderFood(std::promise<void>& promise,SDL_Point const &food, Food::FoodQuality const &quality, SDL_Rect block){
+  std::lock_guard<std::mutex> lock(rendererMutex);  // Lock the mutex
   quality == Food::FoodQuality::High ? SDL_SetRenderDrawColor(sdl_renderer, 0xCC, 0x01, 0x18, 0x1D) 
                                      : SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xCC, 0x00, 0xFF);
   block.x = food.x * block.w;
   block.y = food.y * block.h;
   SDL_RenderFillRect(sdl_renderer, &block);
+}
 
+void Renderer::RenderSnake(std::promise<void>& promise, Snake const snake, SDL_Rect block){
   // Render snake's body
+  std::lock_guard<std::mutex> lock(rendererMutex);
   SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0xFF, 0xFF, 0xFF);                           
   for (SDL_Point const &point : snake.body) {
     block.x = point.x * block.w;
     block.y = point.y * block.h;
     SDL_RenderFillRect(sdl_renderer, &block);
   }
-
+  
   // Render snake's head
   block.x = static_cast<int>(snake.head_x) * block.w;
   block.y = static_cast<int>(snake.head_y) * block.h;
@@ -71,9 +66,31 @@ void Renderer::Render(Snake const snake, SDL_Point const &food, Food::FoodQualit
     SDL_SetRenderDrawColor(sdl_renderer, 0xFF, 0x00, 0x00, 0xFF);
   }
   SDL_RenderFillRect(sdl_renderer, &block);
+}
+
+void Renderer::Render(Snake const snake, SDL_Point const &food, Food::FoodQuality const &quality) {
+
+  SDL_Rect block;
+  block.w = screen_width / grid_width;
+  block.h = screen_height / grid_height;
+
+  // Clear screen
+  SDL_SetRenderDrawColor(sdl_renderer, 0x1E, 0x1E, 0x1E, 0xFF);
+  SDL_RenderClear(sdl_renderer);
+
+  //Render food
+  std::promise<void> promise_food;
+  std::future<void> ftr_food = std::async(std::launch::async, [this,&promise_food, food, quality,block](){this->RenderFood(promise_food,food,quality,block);});
+
+  //Render Snake
+  std::promise<void> promise_snake;
+  std::future<void> ftr_snake = std::async(std::launch::async, [this,&promise_snake, snake, block](){this->RenderSnake(promise_snake,snake,block);});
 
   // Update Screen
   SDL_RenderPresent(sdl_renderer);
+
+  ftr_food.wait();
+  ftr_snake.wait(); 
 }
 
 void Renderer::UpdateWindowTitle(int score, int fps) {
--- src/renderer.h
+++ src/renderer.h
@@ -5,6 +5,7 @@
 #include "SDL.h"
 #include "snake.h"
 #include "food.h"
+#include <future>
 
 class Renderer {
  public:
@@ -12,12 +13,16 @@ class Renderer {
            const std::size_t grid_width, const std::size_t grid_height);
   ~Renderer();
 
+  //void RenderSnake(Snake const snake, SDL_Point const &food, Food::FoodQuality const &quality);
+  void RenderFood(std::promise<void>& promise, SDL_Point const &food, Food::FoodQuality const &quality, SDL_Rect block);
+  void RenderSnake(std::promise<void>& promise, Snake const snake, SDL_Rect block);
   void Render(Snake const snake, SDL_Point const &food, Food::FoodQuality const &quality);
   void UpdateWindowTitle(int score, int fps);
 
  private:
   SDL_Window *sdl_window;
   SDL_Renderer *sdl_renderer;
+  std::mutex rendererMutex;
 
   const std::size_t screen_width;
   const std::size_t screen_height;
